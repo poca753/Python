d5d43979bd481f67287692354f8b4175da1b28aa
poca753 <165678402+poca753@users.noreply.github.com>
2026-03-03 09:16:34 -0300

    kj
    umas coisa ai

--- exercicios/x04.py
+++ exercicios/x04.py
@@ -3,3 +3,8 @@
 #*float é igual numeros com virgula
 #*bool é para armazenar valores verdadeiros e falso
 #*str armazena texto
+
+dia=input("que dia é hoje ?")
+mes=input("que mes é esse ?")
+ano=input("que ano é esse ?")
+print("nossa atividade esta sendo feita em {} de {} em {}".format(dia,mes,ano))
