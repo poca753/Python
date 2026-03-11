2f399a564e82b0bb7476959e1ea7261a9479f2d2
poca753 <165678402+poca753@users.noreply.github.com>
2026-03-11 10:57:49 -0300

    747
    34345

---
+++ exercicios/ex10.py
@@ -0,0 +1,4 @@
+met = int(input("digite um valor em metros "))
+cent = ((met*100))
+mili = ((met*1000))
+print("o valor de {} metros em centimetros é {} e em milimetros é {}".format(met,cent,mili))
---
+++ exercicios/ex11.py
@@ -0,0 +1,2 @@
+n1=int(input("digite seu numero "))
+print("a tabuada de {} é {},{},{},{},{},{},{},{},{}".format(n1,n1*2,n1*3,n1*4,n1*5,n1*6,n1*7,n1*8,n1*9,n1*10))
