336577cf2ee9b2842b9efc3a2802831df4ec6cba
poca753 <165678402+poca753@users.noreply.github.com>
2026-03-16 12:24:05 -0300

    utr6yr
    4e5ry

--- exercicios/ex13.py
+++ exercicios/ex13.py
@@ -1,1 +1,4 @@
-larg = int(input)
+altura = int(input("qual a altura "))
+largura = int(input("qual a largura "))
+area = (altura*largura)
+print("a area em metros quadrados a ser pintada é {}".format(area))
