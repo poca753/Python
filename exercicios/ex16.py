28b95be3d7748031eb48ab99cdd545f181febcb2
poca753 <165678402+poca753@users.noreply.github.com>
2026-03-18 09:01:18 -0300

    6u6ytu
    rrt6uyr

---
+++ exercicios/ex16.py
@@ -0,0 +1,2 @@
+temperatura = float(input("informe a temeperatura °C: "))
+print("A temperatura de {:.2f}°C corresponde a {:.2f}°F ".format(temperatura,(temperatura*(9/5))+32))
---
+++ exercicios/ex17.py
@@ -0,0 +1,4 @@
+Km = float(input("quantos Km o carro percorreu? "))
+tempo = float(input("por quantos dias os carro foi alugado? "))
+soma = (Km*1.15) + (tempo*60)
+print("o total a pagar sera {}".format(soma))
