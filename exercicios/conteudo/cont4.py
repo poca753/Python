6d98bae4fefdbb31aeefc3fb222d34acb4688f7d
poca753 <165678402+poca753@users.noreply.github.com>
2026-03-24 12:26:31 -0300

    rsfsdef
    swada

---
+++ exercicios/conteudo/cont4.py
@@ -0,0 +1,11 @@
+# import math
+# num = int(input("digite um numero"))
+# raiz = math.sqrt(num)
+# print("a raiz de {} é igual a {}" .format(num,math.ceil(raiz)))
+# print("a raiz de {} é igual a {}" .format(num,math. (raiz)))
+
+#importando funcionalidades especificas
+from math import sqrt,floor, ceil
+num = int(input("digite um numero"))
+raiz = sqrt(num)
+print("a raiz de {} é igual a {}".format(num,ceil,raiz))
