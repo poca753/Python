17e58bd047bf0a6691784f30453a1317d028b24e
poca753 <165678402+poca753@users.noreply.github.com>
2026-04-28 09:48:06 -0300

    ewdew
    ewdwea

--- exercicios/conteudo/cont8.py
+++ exercicios/conteudo/cont8.py
@@ -1,6 +1,30 @@
-tempo = int(input("quantos anos tem o seu carro "))
-if tempo <=3:
-    print("carro novo")
+#tempo = int(input("quantos anos tem o seu carro "))
+#if tempo <=3:
+#    print("carro novo")
+#else:
+#    print("carro velho")
+#print("fim")
+
+#tempo = int(input("quantos anos tem o seu carro "))
+#print("carro novo" if tempo <=3 else "carro velho") 
+#print("fim")
+
+#nome=str(input("qual é seu nome "))
+#if nome == "daniel":
+#    print("que nome lindo você tem")
+#else:
+#    print("o seu nome é tao normal")
+#    print("bom dia {}".format(nome))
+#print("bom dia {}".format(nome#))
+
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+m = (n1 + n2) / 2
+print("A sua média foi {:.1f}".format(m))
+
+if m >= 6.0:
+    print("A sua média foi boa! Parabéns")
 else:
-    print("carro velho")
-print("fim")
+    print("Sua média foi ruim! Estude mais")
+
+print("Parabéns" if m >= 6 else "Estude mais")
