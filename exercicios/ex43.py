a71e62d4c8e48c48d7ec450266031ca967514390
poca753 <165678402+poca753@users.noreply.github.com>
2026-05-14 09:52:11 -0300

    rfwe34r
    23r23q

---
+++ exercicios/ex43.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+ano =int(input("digite o ano que voce nasceu "))
+idade = date.today().year - ano
+
+print(f'O atleta tem {idade} anos.')
+
+if idade <= 9:
+    print('Classificação: MIRIM')
+elif idade <= 14:
+    print('Classificação: INFANTIL')
+elif idade <= 19:
+    print('Classificação: JUNIOR')
+elif idade <= 25:
+    print('Classificação: SÊNIOR')
+else:
+    print('Classificação: MASTER')
