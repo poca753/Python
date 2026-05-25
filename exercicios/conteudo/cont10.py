fa35e325715f9cb6e1230be0a76a96b9fc30925e
poca753 <165678402+poca753@users.noreply.github.com>
2026-05-25 11:01:00 -0300

    ew3rw
    3q2t34ww

--- exercicios/conteudo/cont10.py
+++ exercicios/conteudo/cont10.py
@@ -1,15 +1,21 @@
-for c in range(1, 6):
-    print("oi")
-print("fim")
+#for c in range(1, 6):
+#    print("oi")
+#print("fim")
 
-for c in range(1, 7):
-    print(c)
-print("fim")
+#for c in range(1, 7):
+#    print(c)
+#print("fim")
 
-for c in range(6, -1, -1):
-    print(c)
-print("fim")
+#for c in range(6, -1, -1):
+#    print(c)
+#print("fim")
 
-for c in range(0, 6, 2):
-    print(c)
-print("fim")
+#for c in range(0, 6, 2):
+#    print(c)
+#print("fim")
+
+s = 0
+for c in range(0, 4):
+    n = int(input("digite um valor:"))
+    s += n
+print(f"a somatoria de todos os valores foi {s}")
